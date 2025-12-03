3ae2fd6eaabfd5b941b29a53e5ac700c57256762
Raul Ferreira da Rocha <raul.ferreiradarocha@gmail.com>
2025-12-03 20:46:53 -0300

    feat: atualizado lista de perfis precisa ajustar largura da tabela

---
+++ wp-content/themes/soda-perfeita/page-formulario-perfil.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Formulário Perfil Soda Perfeita
+ */
+get_header();
+?>
+<div class="container py-4 py-md-5">
+    <?php echo do_shortcode('[soda_perfeita_perfil_form]'); ?>
+</div>
+<?php
+get_footer();
